5efafe3df0c058bf525e6759aeffb933c84eea09
ashwikadasari <dasariashwika77@gmail.com>
2025-02-18 11:47:06 +0530

    feat: Pushed server-config.js

--- server-config.js
+++ server-config.js
@@ -1,12 +1,12 @@
 // Put here your email sending configurations!
 module.exports = {
-	emailTransport: {
-		// host: 'yoursmtpserver',
-		// port: 25,
-		// auth: { user: '####', pass: '####' }
-	},
-	emailOptions: {
-		from: 'Mosaico by VOXmail <test@mosaico.io>', // sender address
-		// bcc: 'mosaico@mosaico.io',
-	}
+    emailTransport: {
+        host: 'smtp-relay.brevo.com',
+        port: 587,
+        auth: { user: '82bdab002@smtp-brevo.com', pass: 'rv28YjS0cxQ7Nh6J' }
+    },
+    emailOptions: {
+        from: 'Mosaico by VOXmail <krishnaneel_deva@yahoo.com>', // sender address
+        // bcc: 'mosaico@mosaico.io',
+    }
 };
